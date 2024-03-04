0847a5235c3af9030f9bce1c7b5dc984f69b56be
Vijay <vachaudhari26@gmail.com>
2024-03-04 22:32:10 +0530

    Sample Get endoint added

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <iostream>
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+void handle_get(http_request request) {
+    request.reply(status_codes::OK, U("Received GET Request"));
+}
+
+int main() {
+    http_listener listner(U("http://localhost:8082"));
+    listner.support(methods::GET, handle_get);
+
+    try {
+        listner.open()
+            .then([&listner]() {std::cout<<"Starting server";})
+            .wait();
+    } catch(std::exception const& e) {
+        std::cerr<<"Error: "<<e.what()<<std::endl; 
+    }
+    return 0;
+}
